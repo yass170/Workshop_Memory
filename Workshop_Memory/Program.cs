976575f68062c938ef3f6697ed66f37d03ce34b6
Yasser TOUMI <149902910+yass170@users.noreply.github.com>
2026-02-06 09:23:30 +0100

    Create : Delegate function

--- Workshop_Memory/Program.cs
+++ Workshop_Memory/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Workshop_Memory
+{
+    class Program
+    {
+        private delegate int AdditionDelegate(int v1, int v2);
+
+        private static int methode(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        static void Main(string[] args)
+        {
+            AdditionDelegate del = methode;
+            int result = del(12, 30);
+
+            Console.WriteLine($"Resultat: {result}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
