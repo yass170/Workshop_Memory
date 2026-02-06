0672b004c04501c06dd2b5d4a027d9fe6843d618
Yasser TOUMI <149902910+yass170@users.noreply.github.com>
2026-02-06 09:47:17 +0100

    2b

--- Workshop_Memory/Program.cs
+++ Workshop_Memory/Program.cs
@@ -1,22 +1,69 @@
 using System;
+using System.Threading;
 
 namespace Workshop_Memory
 {
     class Program
     {
         private delegate int AdditionDelegate(int v1, int v2);
+        private delegate int SquareDelegate(int v);
 
         private static int methode(int v1, int v2)
         {
             return v1 + v2;
         }
 
-        static void Main(string[] args)
+        private static void RunExercise1()
         {
             AdditionDelegate del = methode;
             int result = del(12, 30);
 
             Console.WriteLine($"Resultat: {result}");
+        }
+
+        private static void RunExercise2a()
+        {
+            SquareDelegate d = x =>
+            {
+                int res = x * x;
+                Console.WriteLine($"The square of {x} is {res}");
+                return res;
+            };
+
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                int j = i; // capture loop value for the closure
+                threads[j] = new Thread(() => d(j));
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+        }
+
+        private static void RunExercise2b()
+        {
+            Func<int, int> square = x => x * x;
+            int value = 7;
+            int result = square(value);
+
+            Console.WriteLine($"Square of {value} is {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            RunExercise1();
+            RunExercise2a();
+            RunExercise2b();
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
